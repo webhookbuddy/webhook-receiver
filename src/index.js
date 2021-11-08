49621b8502bd3c35b3ba19fc7f2bb1d2faac85d3
Johnny Oshika <johnnyoshika@gmail.com>
2021-11-07 21:46:20 -0800

    Format index.js with prettier

--- src/index.js
+++ src/index.js
@@ -2,41 +2,36 @@ const express = require('express');
 const url = require('url');
 const bodyParser = require('body-parser');
 let morgan = require('morgan');
-const cors = require("cors");
-
-
+const cors = require('cors');
 
 let corsOptions = {
-  origin: "*",
+  origin: '*',
 };
 
 const app = express();
-const port = 8080;
+const port = 8000;
 
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev"))
-
+app.use(morgan('dev'));
 
 app.post('/', (req, res) => {
-    // console.log("Got body:", req.body);
-    let statusCode = parseInt(req.query.statuscode) || 200;
-    const delay_ms = parseInt(req.query.delay) || 0;
-    
-    if (delay_ms !== undefined) {
-        setTimeout(() => {
-            res.status(statusCode).json({ msg: "hello" });
-        }, delay_ms);
-    }
+  let statusCode = parseInt(req.query.statuscode) || 200;
+  const delay_ms = parseInt(req.query.delay) || 0;
+
+  if (delay_ms !== undefined) {
+    setTimeout(() => {
+      res.status(statusCode).json({ msg: 'hello' });
+    }, delay_ms);
+  }
 });
 
-app.get("/", (req, res) => {
-    res.send("test");
-  });
+app.get('/', (req, res) => {
+  res.send('test');
+});
 
 app.listen(port, () => {
-    console.log(`Running a API server at http://localhost:${port}/`);
+  console.log(`Running a API server at http://localhost:${port}/`);
 });
-
